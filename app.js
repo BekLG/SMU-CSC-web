3f5761caff153d43778f829536ded279eb7733b1
BekLg <bereketlegesse22@gmail.com>
2023-06-27 13:41:06 +0300

    created connection with DB

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ const mongoose = require("mongoose");
 const lodash = require("lodash");
 const findOrCreate = require('mongoose-findorcreate');
 const path = require("path");
+const { log } = require("console");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/CSC-web", { useNewUrlParser: true });
+
+
+
+app.get("/", function(req,res){
+    res.send("home page");
+})
+
+
+
+
+
+
+app.listen(3000, function(req,res){
+    console.log("listening on port 3000");
+})
