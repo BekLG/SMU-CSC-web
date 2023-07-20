62a42b5ca5492101e3269780b5b03f8e3a70a419
BekLg <bereketlegesse22@gmail.com>
2023-07-20 08:30:55 +0300

    installed nad configured cloudinary

--- modules/multerMiddleware.js
+++ modules/multerMiddleware.js
@@ -1,20 +1,32 @@
 const multer = require('multer');
 
-// Define the storage configuration for multer
+// // Define the storage configuration for multer
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     // Specify the destination folder where the images will be stored
+//     cb(null, 'public/uploads/images');
+//   },
+//   filename: function (req, file, cb) {
+//     // Generate a unique filename for each uploaded image
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     const extension = file.originalname.split('.').pop();
+//     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
+//   },
+// });
+
+// // Create the multer middleware instance
+// const upload = multer({ storage: storage });
+
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Specify the destination folder where the images will be stored
-    cb(null, 'public/uploads/images');
+    cb(null, './uploads'); // Destination folder for uploaded images on the server
   },
   filename: function (req, file, cb) {
-    // Generate a unique filename for each uploaded image
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const extension = file.originalname.split('.').pop();
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
-  },
+    cb(null, Date.now() + '-' + file.originalname);
+  }
 });
 
-// Create the multer middleware instance
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 module.exports = upload;
